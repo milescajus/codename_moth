4b89eb3860bb382214af27707564ffd0b710bf23
Ricarto <gurwls613@gmail.com>
2021-11-05 15:14:54 -0500

    MainMenu + Scene Transfer added

---
+++ Luminaria/Assets/Scripts/SceneButton.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButton : MonoBehaviour
+{
+    [SerializeField] private string nextSceneName;
+    [SerializeField] private string CurrentScene;
+
+    public void loadCredit()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exit the Game");
+    }
+
+    public void backToMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void reStart()
+    {
+        SceneManager.LoadScene(CurrentScene);
+    }
+
+    public void nextStage()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
