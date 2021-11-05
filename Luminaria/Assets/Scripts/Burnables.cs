22e83926781e4b5b137c3d1faf5bf7a8215397e3
milescajus <80205080+milescajus@users.noreply.github.com>
2021-11-05 13:45:05 -0500

    fixed burning script

--- Luminaria/Assets/Scripts/Burnables.cs
+++ Luminaria/Assets/Scripts/Burnables.cs
@@ -3,25 +3,26 @@ using UnityEngine;
 public class Burnables : MonoBehaviour
 {
     [SerializeField] private bool triggerActive = false;
+    [SerializeField] public CharacterController2D Aspen;
 
-    public void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) {
+        if (other.gameObject.CompareTag("Aspen")) {
             triggerActive = true;
         }
     }
 
-    public void OnTriggerExit(Collider other)
+    public void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) {
+        if (other.gameObject.CompareTag("Aspen")) {
             triggerActive = false;
         }
     }
 
     private void Update()
     {
-        if (triggerActive && (Input.GetKeyDown(KeyCode.LeftShift)) || Input.GetKeyDown(KeyCode.RightShift)) {
-            Destroy(gameObject);
+        if (triggerActive && Aspen.isBurning) {
+            Destroy(gameObject, 1);
         }
     }
 }
