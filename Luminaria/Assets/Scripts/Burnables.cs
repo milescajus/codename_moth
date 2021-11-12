4c4a236f7cebffb6ccaa62a1d9aeec2d1a9b903a
milescajus <80205080+milescajus@users.noreply.github.com>
2021-11-12 11:47:00 -0600

    added rudimentary burning effect

--- Luminaria/Assets/Scripts/Burnables.cs
+++ Luminaria/Assets/Scripts/Burnables.cs
@@ -7,6 +7,13 @@ public class Burnables : MonoBehaviour
     [SerializeField] private int ChargeCost = 1;
     private bool hasDepleted = false;
 
+    public void Start()
+    {
+        GameObject particles = transform.GetChild(1).transform.GetChild(0).gameObject;
+        particles.GetComponent<ParticleSystem> ().GetComponent<Renderer> ().sortingLayerName = "Foreground";
+        particles.GetComponent<ParticleSystem> ().GetComponent<Renderer> ().sortingOrder = 21;
+    }
+
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Aspen")) {
@@ -24,6 +31,8 @@ public class Burnables : MonoBehaviour
     private void Update()
     {
         if (triggerActive && Aspen.isBurning && (Aspen.chargeLevel != 0)) {
+            transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(2).gameObject.SetActive(true);
             Destroy(gameObject, 1);
 
             if (!hasDepleted) {
