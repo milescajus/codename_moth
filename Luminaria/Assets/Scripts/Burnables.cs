e20ac0c7806a7f7a0da4182ca0b947279039d428
milescajus <80205080+milescajus@users.noreply.github.com>
2021-11-12 12:33:59 -0600

    added burning audio clip

--- Luminaria/Assets/Scripts/Burnables.cs
+++ Luminaria/Assets/Scripts/Burnables.cs
@@ -5,13 +5,17 @@ public class Burnables : MonoBehaviour
     [SerializeField] private bool triggerActive = false;
     [SerializeField] public CharacterController2D Aspen;
     [SerializeField] private int ChargeCost = 1;
+    private bool hasPlayedClip = false;
     private bool hasDepleted = false;
+    AudioSource soundClip;
+    GameObject fire;
 
     public void Start()
     {
-        GameObject particles = transform.GetChild(1).transform.GetChild(0).gameObject;
-        particles.GetComponent<ParticleSystem> ().GetComponent<Renderer> ().sortingLayerName = "Foreground";
-        particles.GetComponent<ParticleSystem> ().GetComponent<Renderer> ().sortingOrder = 21;
+        GameObject fire = transform.GetChild(1).transform.GetChild(0).gameObject;
+        fire.GetComponent<ParticleSystem> ().GetComponent<Renderer> ().sortingLayerName = "Foreground";
+        fire.GetComponent<ParticleSystem> ().GetComponent<Renderer> ().sortingOrder = 21;
+        soundClip = GetComponent<AudioSource>();
     }
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -31,9 +35,17 @@ public class Burnables : MonoBehaviour
     private void Update()
     {
         if (triggerActive && Aspen.isBurning && (Aspen.chargeLevel != 0)) {
-            transform.GetChild(1).gameObject.SetActive(true);
-            transform.GetChild(2).gameObject.SetActive(true);
+            if (!hasPlayedClip) {
+                soundClip.Play();
+                transform.GetChild(1).gameObject.SetActive(true);
+                transform.GetChild(2).gameObject.SetActive(true);
+                hasPlayedClip = true;
+            }
+
             Destroy(gameObject, 1);
+            // Destroy(transform.GetChild(0).gameObject, 1);
+
+            // fire.GetComponent<ParticleSystem>().Stop();
 
             if (!hasDepleted) {
                 Aspen.chargeLevel -= ChargeCost;
