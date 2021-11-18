1012d5119a5db0a060ed3d88f00c5e23668e58b4
qnf613 <gurwls613@gmail.com>
2021-11-18 14:17:49 -0600

    Guide UI + small collider fix in tutorial scene

---
+++ Luminaria/Assets/Scripts/GuideUI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideUI : MonoBehaviour
+{
+    [SerializeField] private GameObject ThingToGuide;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ThingToGuide.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Aspen")
+        {
+            ThingToGuide.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Aspen")
+        {
+            ThingToGuide.SetActive(false);
+        }
+    }
+}
