56c2e8bc6250fe55c03d67e301e5142724b789bb
qnf613 <gurwls613@gmail.com>
2021-12-05 16:51:43 -0600

    temp pick ups added (charge particles) & bush's burn cost adjusted

---
+++ Luminaria/Assets/Scripts/PickUps.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUps : MonoBehaviour
+{
+    [SerializeField] public CharacterController2D Aspen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Aspen"))
+        {
+            if (gameObject.CompareTag("ChargeParticle") && Aspen.chargeLevel < 3)
+            {
+                Aspen.chargeLevel += 1;
+            }
+
+            //else if (gameObject.CompareTag(""))
+            //{
+
+            //}
+
+            Destroy(gameObject);
+        }
+    }
+}
