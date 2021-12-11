95133a39a43344a111ac5544b58185ff73d221d2
milescajus <80205080+milescajus@users.noreply.github.com>
2021-12-11 15:03:33 +0100

    renamed ChargeParticle prefab, switched to programmatic pulsing from animator

--- Luminaria/Assets/Scripts/PickUps.cs
+++ Luminaria/Assets/Scripts/PickUps.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
@@ -8,18 +9,42 @@ public class PickUps : MonoBehaviour
     [SerializeField] int chargeValue;
     private Light2D lt;
     private SpriteRenderer r;
+    private Vector3 shrinkFactor;
+    private Vector3 rotateFactor;
+    private float pulseTime;
+    private float pulsePeriod;
 
     void Start()
     {
         lt = GetComponentInChildren<Light2D>(true);
         r = GetComponent<SpriteRenderer>();
+        shrinkFactor = transform.localScale / 5;
+        rotateFactor = new Vector3(0, 0, -1.0f);
+        pulseTime = 0f;
+        pulsePeriod = 0.03f;
+
+        if (gameObject.CompareTag("LightOrb"))
+            StartCoroutine(Pulse());
+    }
+
+    private IEnumerator Pulse()
+    {
+        while(true) {
+            transform.Rotate(rotateFactor);
+
+            float ft = (float) (0.37f + 0.05*Math.Sin(pulseTime));       // periodic function for smooth pulsing
+            transform.localScale = new Vector3(ft, ft, ft);
+            pulseTime += pulsePeriod;
+
+            yield return null;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Aspen")) {
 
-            if (gameObject.CompareTag("ChargeParticle")) {
+            if (gameObject.CompareTag("LightOrb")) {
                 Aspen.currentCharge += chargeValue;
             }
 
@@ -46,6 +71,7 @@ public class PickUps : MonoBehaviour
         for (float ft = lt.intensity; ft >= 0; ft -= 1.2f) {
             if (lt != null)
                 lt.intensity = ft;
+            transform.localScale -= shrinkFactor;
 
             yield return null;
         }
