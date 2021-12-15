97289f995fac71bef7f3f6cac70ec5d3b717311f
milescajus <80205080+milescajus@users.noreply.github.com>
2021-12-15 22:52:13 +0100

    added fading to guide UI

--- Luminaria/Assets/Scripts/GuideUI.cs
+++ Luminaria/Assets/Scripts/GuideUI.cs
@@ -1,34 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GuideUI : MonoBehaviour
 {
     [SerializeField] private GameObject ThingToGuide;
-    
-  
-
-    
+    [SerializeField] bool destroyOnFadeOut;
+    private Coroutine fading;
 
     // Start is called before the first frame update
     private void Start()
     {
-        ThingToGuide.SetActive(false);
-       
+        Color box = GetComponentInChildren<SpriteRenderer>().color;
+        Color text = GetComponentInChildren<TMP_Text>().color;
+        box.a = 0;
+        text.a = 0;
+        GetComponentInChildren<SpriteRenderer>().color = box;
+        GetComponentInChildren<TMP_Text>().color = text;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Aspen")
         {
-            ThingToGuide.SetActive(true);
-            
+            fading = StartCoroutine(FadeIn());
         }
     }
 
@@ -36,7 +38,50 @@ public class GuideUI : MonoBehaviour
     {
         if (collision.gameObject.tag == "Aspen")
         {
-            ThingToGuide.SetActive(false);
+            fading = StartCoroutine(FadeOut());
         }
+    }
+
+    IEnumerator FadeIn()
+    {
+        if (fading != null)     // currently fading
+            StopCoroutine(fading);
+
+        Color start = GetComponentInChildren<SpriteRenderer>().color;
+
+        for (float ft = start.a; ft < 1f; ft += 0.1f)
+        {
+            Color box = GetComponentInChildren<SpriteRenderer>().color;
+            Color text = GetComponentInChildren<TMP_Text>().color;
+            box.a = ft;
+            text.a = ft;
+            GetComponentInChildren<SpriteRenderer>().color = box;
+            GetComponentInChildren<TMP_Text>().color = text;
+
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        if (fading != null)     // currently fading
+            StopCoroutine(fading);
+
+        Color start = GetComponentInChildren<SpriteRenderer>().color;
+
+        for (float ft = start.a; ft >= 0f; ft -= 0.1f)
+        {
+            Color box = GetComponentInChildren<SpriteRenderer>().color;
+            Color text = GetComponentInChildren<TMP_Text>().color;
+            box.a = ft;
+            text.a = ft;
+            GetComponentInChildren<SpriteRenderer>().color = box;
+            GetComponentInChildren<TMP_Text>().color = text;
+
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        if (destroyOnFadeOut)
+            Destroy(gameObject);
     }
 }
